e32ed7d7537dbec81637f1099629a771d927f284
Gavrysh <zhenya.ga@gmail.com>
2026-03-12 12:09:49 -0700

    alvins solution to pair sum problem

--- Sources/structlyC/02_hashing/pairSum2.cpp
+++ Sources/structlyC/02_hashing/pairSum2.cpp
@@ -1,5 +1,5 @@
 //
-//  pairSum.cpp
+//  pairSum2.h
 //  MyCLI
 //
 //  Created by new on 3/12/26.
@@ -9,16 +9,16 @@
 #include <vector>
 #include <unordered_map>
 
-std::array<int, 2> pairSum(std::vector<int> numbers, int target) {
+std::array<int, 2> pairSum2(std::vector<int> numbers, int target) {
     std::unordered_map<int, int> m{};
     for (int i=0; i<numbers.size(); i++) {
         int num = numbers[i];
         int complement = target - num;
         if (m.find(complement) != m.end()) {
-            return {m[complement], i};
+            return std::array<int, 2>{m[complement], i};
         }
         m[num] = i;
     }
-    
+
     return {-1,-1};
 }
