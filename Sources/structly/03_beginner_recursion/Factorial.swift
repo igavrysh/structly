732b07441c8fd486c45f6808d6816e651ac7493f
Gavrysh <zhenya.ga@gmail.com>
2026-03-14 14:29:13 -0700

    factorial swift + cpp

--- Sources/structly/03_beginner_recursion/Factorial.swift
+++ Sources/structly/03_beginner_recursion/Factorial.swift
@@ -7,9 +7,9 @@
 
 func factorial(_ n: Int) -> Int64 {
     func f(n: Int, res: Int64) -> Int64 {
-        if n == 0 { return 1 }
-        if n == 1 { return res }
-        return f(n: n-1, res: res * Int64(n))
+        if n == 0 { return res }
+        return f(n: n - 1, res: res * Int64(n))
     }
     return f(n: n, res: 1)
 }
+
