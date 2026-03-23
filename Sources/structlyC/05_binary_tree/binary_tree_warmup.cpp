a657734136f03d05032e5722a69e6c0394123734
Gavrysh <zhenya.ga@gmail.com>
2026-03-23 14:19:37 -0700

    small refactoring, add binary tree warmup cpp structure

---
+++ Sources/structlyC/05_binary_tree/binary_tree_warmup.cpp
@@ -0,0 +1,46 @@
+//
+//  binary_tree_warmup.cpp
+//  MyCLI
+//
+//  Created by new on 3/23/26.
+//
+
+#include <string>
+
+class Node {
+public:
+    Node* left;
+    Node* right;
+    std::string val;
+
+    Node(std::string initialVal) {
+        val = initialVal;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+void run() {
+    Node a("A");
+    Node b("B");
+    Node c("C");
+    Node d("D");
+    Node e("E");
+    Node f("F");
+
+    a.left = &b;
+    a.right = &c;
+    b.left = &d;
+    b.right = &e;
+    c.right = &f;
+}
+
+/*
+ A
+ / \
+ B  C
+ / \   \
+ D   E    F
+
+ */
+
