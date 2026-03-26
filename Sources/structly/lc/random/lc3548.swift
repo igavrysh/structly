ade43be0966c084cca4e3bd8515a74c43e7203c5
Gavrysh <zhenya.ga@gmail.com>
2026-03-26 16:42:30 -0700

    morning session structy

---
+++ Sources/structly/lc/random/lc3548.swift
@@ -0,0 +1,12 @@
+//
+//  lc3548.swift
+//  MyCLI
+//
+//  Created by new on 3/26/26.
+//
+
+class Solution {
+    func canPartitionGrid(_ grid: [[Int]]) -> Bool {
+
+    }
+}
---
+++ Sources/structlyC/05_binary_tree/tree_path_finder.cpp
@@ -0,0 +1,163 @@
+//
+//  tree_path_finder.cpp
+//  MyCLI
+//
+//  Created by new on 3/26/26.
+//
+
+#include <vector>
+#include <string>
+#include <queue>
+#include <tuple>
+
+using namespace std;
+
+class Node {
+public:
+    std::string val;
+    Node* left;
+    Node* right;
+
+    Node(std::string initialVal) {
+        val = initialVal;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+bool path_finder(Node* node, std::vector<std::string> *acc, std::string targetVal);
+
+vector<string>* pathFinder(Node* root, string targetVal) {
+    if (root == nullptr) {
+        return nullptr;
+    }
+
+    queue<tuple<Node*, vector<string>*>> q{};
+    vector<string>* root_acc = new vector<string>();
+    root_acc->push_back(root->val);
+    q.push(tuple<Node*, vector<string>*>(root, root_acc));
+
+    vector<string>* res = nullptr;
+    while (!q.empty()) {
+        tuple<Node*, vector<string>*> ctuple = q.front();
+        q.pop();
+        Node* node = get<0>(ctuple);
+        vector<string>* node_acc = get<1>(ctuple);
+
+        if (node->val == targetVal && res == nullptr) {
+            res = get<1>(ctuple);
+            continue;
+        }
+
+        if (res != nullptr || (node->left == nullptr && node->right == nullptr)) {
+            delete node_acc;
+            continue;
+        }
+
+        if (node->left != nullptr) {
+            vector<string>* new_node_acc = new vector<string>(*node_acc);
+            new_node_acc->push_back(node->left->val);
+            q.push(tuple<Node*, vector<string>*>(node->left, new_node_acc));
+        }
+
+        if (node->right != nullptr) {
+            vector<string>* new_node_acc = new vector<string>(*node_acc);
+            new_node_acc->push_back(node->right->val);
+            q.push(tuple<Node*, vector<string>*>(node->right, new_node_acc));
+        }
+    }
+
+    return res;
+}
+
+std::vector<std::string>* pathFinder1(Node* root, std::string targetVal) {
+    std::vector<std::string> *acc = new std::vector<std::string>();
+    if (!path_finder(root, acc, targetVal)) {
+        return nullptr;
+    }
+
+    return acc;
+}
+
+bool path_finder(Node* node, std::vector<std::string> *acc, std::string targetVal) {
+
+    if (node == nullptr) {
+        return false;
+    }
+
+    if (node->val == targetVal) {
+        acc->push_back(node->val);
+        return true;
+    }
+
+    acc->push_back(node->val);
+
+    bool res = path_finder(node->left, acc, targetVal);
+    if (res == true) {
+        return true;
+    }
+
+    res = path_finder(node->right, acc, targetVal);
+    if (res == true) {
+        return true;
+    }
+    acc->pop_back();
+
+    return false;
+}
+
+void tree_path_finder_test_00() {
+    Node a("a");
+    Node b("b");
+    Node c("c");
+    Node d("d");
+    Node e("e");
+    Node f("f");
+
+    a.left = &b;
+    a.right = &c;
+    b.left = &d;
+    b.right = &e;
+    c.right = &f;
+
+    //      a
+    //    /   \
+    //   b     c
+    //  / \     \
+    // d   e     f
+
+    vector<string>* res = pathFinder(&a, "e"); // -> [ "a", "b", "e" ]
+    int t = 1;
+}
+
+void tree_path_finder_test_07() {
+    Node* root = new Node("0");
+    Node* curr = root;
+    for (int i = 1; i <= 20000; i += 1) {
+        curr->right = new Node(std::to_string(i));
+        curr = curr->right;
+    }
+
+    //      0
+    //       \
+    //        1
+    //         \
+    //          2
+    //           .
+    //            .
+    //             .
+    //            19999
+    //              \
+    //              20000
+
+    vector<string>* res = pathFinder(root, "19000"); // -> [0, 1, 2, ..., 18999, 19000]
+    int t = 1;
+}
+
+int main(int argc, char const *argv[]) {
+
+    tree_path_finder_test_07();
+    
+    return 0;
+}
+
