968d981eac2cc08fe4d993fdc06262a9cf9ba2ab
Gavrysh <zhenya.ga@gmail.com>
2026-03-30 09:06:42 -0700

    lc daily

---
+++ Sources/lc/random/lc2840.swift
@@ -0,0 +1,38 @@
+//
+//  lc2840.swift
+//  MyCLI
+//
+//  Created by new on 3/30/26.
+//
+
+class Solution {
+    func checkStrings(_ s1: String, _ s2: String) -> Bool {
+        var s1 = Array(s1)
+        var s2 = Array(s2)
+        var oddFqs: Dictionary<Character, Int> = [:]
+        var evenFqs: Dictionary<Character, Int> = [:]
+        for (i, c) in s1.enumerated() {
+            if i%2==0 {
+                evenFqs[c, default: 0] += 1
+            } else {
+                oddFqs[c, default: 0] += 1
+            }
+        }
+
+        for (i, c) in s2.enumerated() {
+            if i%2==0 {
+                if evenFqs[c, default: 0] == 0 {
+                    return false
+                }
+                evenFqs[c, default: 0] -= 1
+            } else {
+                if oddFqs[c, default: 0] == 0 {
+                    return false
+                }
+                oddFqs[c, default: 0] -= 1
+            }
+        }
+
+        return true
+    }
+}
