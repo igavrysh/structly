47b449ea8f722c52c0f32fddd57499503fb4bcdc
Gavrysh <zhenya.ga@gmail.com>
2026-04-06 00:46:04 -0700

    is_palindrome problem structy

---
+++ Sources/structyC/08_two_pointer/is_palindrome.cpp
@@ -0,0 +1,22 @@
+//
+//  is_palindrome.cpp
+//  MyCLI
+//
+//  Created by new on 4/6/26.
+//
+
+#include <string>
+
+bool isPalindrome(std::string s) {
+    int l = 0;
+    int r = s.length()-1;
+    while (l<r) {
+        if (s[l] != s[r]) {
+            return false;
+        }
+        l++;
+        r--;
+    }
+    
+    return true;
+}
