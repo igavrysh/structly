952a6d8e70cdc1230bcdc1fe235d1347ccb4adaa
Gene <zhenya.ga@gmail.com>
2026-04-11 16:53:42 -0700

    structy longest two char substring

--- Sources/structyC/07_sliding_window/longest_two_char_substring.cpp
+++ Sources/structyC/07_sliding_window/longest_two_char_substring.cpp
@@ -8,15 +8,22 @@
 #include <string>
 #include <unordered_set>
 #include <algorithm>
+#include <iostream>
+
 using namespace std;
 
 int longestTwoCharSubstring(std::string s) {
     unordered_map<char, int> map{};
-    int res = -1;
+    int res = 0;
     int l = 0;
     for (int r = 0; r < s.length(); r++) {
         char ch = s[r];
-        while (map.size()>2 && l<=r) {
+        if (map.count(ch) == 0) {
+            map[ch] = 0;
+        }
+        map[ch]++;
+
+        while (map.size() > 2 && l <= r) {
             char ch_r = s[l++];
             map[ch_r]--;
             if (map[ch_r] == 0) {
@@ -24,8 +31,10 @@ int longestTwoCharSubstring(std::string s) {
             }
         }
 
-        map[ch]++;
-        res = max(res, r-l+1);
+        if (map.size()==2) {
+            res = max(res, r-l+1);
+        }
     }
+    
     return res;
 }
