a62ed8709a7ebd6e3c8f2bd43d3489c6209198dd
Gavrysh <ievgen.gavrysh@gmail.com>
2026-05-01 12:45:28 -0700

    create combs

--- Sources/structyC/16_exhaustive_recursion/create_combinations.cpp
+++ Sources/structyC/16_exhaustive_recursion/create_combinations.cpp
@@ -6,6 +6,22 @@
 #include <string>
 using namespace std;
 
+void create_combinations(int i, vector<string>& acc, vector<string>& els, vector<vector<string>>& res, int size) {
+    if (acc.size() == size) {
+        res.push_back(acc);
+        return;
+    }
+
+    for (int j = i; j < els.size(); j++) {
+        vector<string> next_acc(acc.begin(), acc.end());
+        next_acc.push_back(els[j]);
+        create_combinations(j+1, next_acc, els, res, size);
+    }
+}
+
 vector<vector<string>> createCombinations(vector<string> items, int size) {
-    return nullptr;
+    vector<vector<string>> res{};
+    vector<string> acc{};
+    create_combinations(0, acc, items, res, size);;
+    return res;
 }
