e28f4250986fd226d91d8624bd7a112b929d55ef
Gene Gavrysh <ievgen.gavrysh@gmail.com>
2026-04-30 19:54:21 -0700

    broken permutations

--- Sources/structyC/16_exhaustive_recursion/permutations.cpp
+++ Sources/structyC/16_exhaustive_recursion/permutations.cpp
@@ -4,9 +4,30 @@
 
 #include <vector>
 #include <string>
+#include <algorithm>
+#include <iostream>
 using namespace std;
 
 vector<vector<string>> permutations(vector<string> elements) {
+    if (elements.empty()) {
+        return {{}};
+    }
+
+    string el = elements[0];
+    vector<string> elsWoEl(elements.begin() + 1, elements.end());
+    vector<vector<string>> res = permutations(elsWoEl);
+    for (vector<string>& r : res) {
+        int r_size = r.size();
+        for (int i = 0; i <= r_size; i++) {
+            vector<string> new_comb(r.begin(), r.end());
+            new_comb.insert(new_comb.begin() + i, el);
+            res.push_back(new_comb);
+        }
+    }
+    return res;
+}
+
+vector<vector<string>> permutations_sel_val_for_pos(vector<string> elements) {
     if (elements.empty()) {
         return vector<vector<string>>{{}};
     }
@@ -15,7 +36,7 @@ vector<vector<string>> permutations(vector<string> elements) {
     for (int i = 0; i < elements.size(); i++) {
         vector<string> elWo(elements.begin(), elements.end());
         elWo.erase(elWo.begin() + i);
-        vector<vector<string>> r = permutations(elWo);
+        vector<vector<string>> r = permutations_sel_val_for_pos(elWo);
         for (vector<string>& perm : r) {
             perm.push_back(elements[i]);
             res.push_back(perm);
@@ -23,4 +44,32 @@ vector<vector<string>> permutations(vector<string> elements) {
     }
 
     return res;
+}
+
+void sort_2d(vector<vector<string>>& res) {
+    for (vector<string>& r : res) {
+        sort(r.begin(), r.end());
+    }
+    sort(res.begin(), res.end());
+}
+
+void test_00() {
+    vector<vector<string>> res = permutations(vector<string> { "a", "b", "c" });
+    sort_2d(res);
+    vector<vector<string>> exp_res = {
+        { "a", "b", "c" }, 
+        { "b", "a", "c" }, 
+        { "b", "c", "a" }, 
+        { "a", "c", "b" }, 
+        { "c", "a", "b" }, 
+        { "c", "b", "a" } 
+    };
+    sort_2d(exp_res);
+    bool passed = res == exp_res;
+    cout << "test_00: " << (passed ? "passed" : "failed") << endl;
+}
+
+int main(int argc, char const *argv[]) {
+    test_00();
+    return 0;
 }
