746055f6203bd34a0467fdb26161f89fb79fbe6b
Gavrysh <ievgen.gavrysh@gmail.com>
2026-05-01 14:11:24 -0700

    combinations - following structy approach

--- Sources/structyC/16_exhaustive_recursion/create_combinations.cpp
+++ Sources/structyC/16_exhaustive_recursion/create_combinations.cpp
@@ -6,7 +6,27 @@
 #include <string>
 using namespace std;
 
-void create_combinations(int i, vector<string>& acc, vector<string>& els, vector<vector<string>>& res, int size) {
+vector<vector<string>> createCombinations(vector<string> items, int size) {
+    if (size == 0) {
+        return {{}};
+    }
+    if (items.empty()) {
+        return {};
+    }
+
+    string elem = items[0];
+    vector<string> itemswo(items.begin() + 1, items.end());
+    vector<vector<string>> res = createCombinations(itemswo, size);
+    vector<vector<string>> res_size_reduced = createCombinations(itemswo, size-1);
+    for (vector<string> str : res_size_reduced) {
+        str.push_back(elem);
+        res.push_back(str);
+    }
+
+    return res;
+}
+
+void create_combinations_optimized(int i, vector<string>& acc, vector<string>& els, vector<vector<string>>& res, int size) {
     if (acc.size() == size) {
         res.push_back(acc);
         return;
@@ -15,13 +35,13 @@ void create_combinations(int i, vector<string>& acc, vector<string>& els, vector
     for (int j = i; j < els.size(); j++) {
         vector<string> next_acc(acc.begin(), acc.end());
         next_acc.push_back(els[j]);
-        create_combinations(j+1, next_acc, els, res, size);
+        create_combinations_optimized(j+1, next_acc, els, res, size);
     }
 }
 
-vector<vector<string>> createCombinations(vector<string> items, int size) {
+vector<vector<string>> createCombinationsOpt(vector<string> items, int size) {
     vector<vector<string>> res{};
     vector<string> acc{};
-    create_combinations(0, acc, items, res, size);;
+    create_combinations_optimized(0, acc, items, res, size);;
     return res;
 }
