4b5c4e66c2bf219f7781895aacfd9e90ec56d3b7
Gavrysh <ievgen.gavrysh@gmail.com>
2026-05-07 20:47:02 -0700

    token replace - structy

--- Sources/structyC/18_mixed_recall/breaking_boundaries.cpp
+++ Sources/structyC/18_mixed_recall/breaking_boundaries.cpp
@@ -1,3 +1,7 @@
 //
 // Created by new on 5/7/26.
 //
+
+int breakingBoundaries(int m, int n, int k, int r, int c) {
+    return -1;
+}
--- Sources/structyC/18_mixed_recall/token_replace.cpp
+++ Sources/structyC/18_mixed_recall/token_replace.cpp
@@ -1,3 +1,32 @@
 //
 // Created by new on 5/7/26.
 //
+
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+string tokenReplace(string s, unordered_map<string, string> tokens) {
+    string output;
+    int i = 0;
+    int j = 1;
+    while (i < s.length()) {
+        if (s[i] != '$') {
+            output += s[i];
+            i += 1;
+            j = i + 1;
+        } else if (s[j] != '$') {
+            j += 1;
+        } else {
+            string key = s.substr(i, j-i+1);
+            if (tokens.count(key) == 0) {
+                return "";
+            }
+            output += tokens[key];
+            i = j + 1;
+            j = i + 1;
+        }
+    }
+
+    return output;
+}
